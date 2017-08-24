dda374e988c52ab285c5fccb6e3e2e804224df2d
hisayohorie <hisayohorie@gmail.com>
2017-08-24 14:15:25 -0400

    Added back admin/suggested_councillors.rb
    To download csv file of suggested_councillors which belong to one
    councillor contribution in the admin/councillor_contribution#show page,
    we need admin/suggested_councillors.rb

---
+++ app/admin/suggested_councillors.rb
@@ -0,0 +1,10 @@
+ActiveAdmin.register SuggestedCouncillor do
+ belongs_to :councillor_contribution
+
+ filter :councillor_contribution, :collection => proc { @councillor_contribution.suggested_councillors}
+  csv do
+    column :name
+    column :email
+  end
+
+end
